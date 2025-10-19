27c575afc013f82f73a92578ef50600d86dbed45
Antonio Batarilović <antoniobnoni@gmail.com>
2025-10-20 01:08:00 +0200

    add tailwind support, create error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import Nav from "./components/Nav"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import ErrorPage from "./pages/ErrorPage"
+
 function App() {
 
   return (
-    <div>
-      <h1>progi projekt</h1>
-    </div>
+    <BrowserRouter>
+      <main className="relative">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/error" element={<ErrorPage />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 sm:py-10 absolute z-10 w-full">
+      <nav className="max-container">
+        <a className="font-red-hat text-2xl sm:text-[32px]" href="/">
+          <span className="font-bold text-primary">Gear</span>
+          <span className="text-white">Share</span>
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
