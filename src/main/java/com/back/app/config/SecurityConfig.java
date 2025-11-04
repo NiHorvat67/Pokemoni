761f813b8a26af6d5e577acfb9e7ef8950425518
Ubuntu <ubuntu@ip-172-31-23-138.eu-north-1.compute.internal>
2025-11-04 09:34:05 +0000

    config for deploy

--- src/main/java/com/back/app/config/SecurityConfig.java
+++ src/main/java/com/back/app/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+
 import com.back.app.service.OAuthRoleService;
 
 @Configuration
