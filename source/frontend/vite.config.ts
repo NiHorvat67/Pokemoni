1bde11de9a36f5e8a4b02a3c1d92b89f01cceef6
Nikola Horvat <nikolahorvat2004@gmail.com>
2025-12-16 15:51:38 +0100

    change backend source

--- source/frontend/vite.config.ts
+++ source/frontend/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://51.21.192.233:8080',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '')
       }
