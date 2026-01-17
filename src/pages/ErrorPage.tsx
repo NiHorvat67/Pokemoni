76109a09d80f6a8b702d70e26031f896dc164172
Antonio Batarilović <antoniobnoni@gmail.com>
2026-01-17 20:41:20 +0100

    add animation to error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,18 +1,22 @@
 import { homeIcon } from "../assets/icons"
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
 
 const ErrorPage = () => {
   return (
     <section className="relative padding-x pt-40 sm:pt-60 min-h-screen">
       <section className="absolute w-full h-full left-0 right-0 top-0 bottom-0-0 bg-hero bg-cover bg-center"></section>
-      <section className="absolute w-full h-full left-0 right-0 top-0 bottom-0-0 bg-[#101211]/[90%]"></section>
+      <section className="absolute w-full h-full left-0 right-0 top-0 bottom-0-0 bg-[#101211]/[93%]"></section>
       <section className="max-container relative">
-        i
-        <div className="flex items-start flex-col">
-          <h1 className="font-red-hat text-[32px] sm:text-[40px] font-bold mb-5">
-            <span className="text-primary">404</span>
-            <span className="text-white"> Not Found</span>
-          </h1>
-          <p className="font-inter text-white text-[16px] mb-6">Sorry we are unable to find the page</p>
+
+        <div className="flex items-center flex-col">
+          <DotLottieReact
+            className="max-w-[900px]"
+            src="https://lottie.host/0afe0385-5779-44e2-b18d-497aa61ac028/TKdG2Imnlh.lottie"
+            loop
+            autoplay
+          />
+          <p className="mt-8 font-inter text-white text-[16px] mb-6 text-center">Sorry we are unable to find the page</p>
           <a className="font-inter flex gap-2 px-[14px] py-[6px] bg-primary rounded-[8px]  hover:border-primary hover:shadow-[0_0_15px_#00FA55] transition-shadow duration-300" href="/">
             <img src={homeIcon} alt="home icon" />
             Go Home
